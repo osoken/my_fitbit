d90805b17140a6447bb15403e4d4fb9118121300
osoken <osoken.devel@outlook.jp>
2016-01-09 16:05:00 +0900

    added config module 	modified:   .gitignore 	new file:   fbv/config.js 	modified:   fbv/routes/index.js

---
+++ fbv/config.js
@@ -0,0 +1,18 @@
+var config = {
+  'fbOauth2AuthURI': 'https://www.fitbit.com/oauth2/authorize',
+  'fbOauth2RefreshURI': 'https://api.fitbit.com/oauth2/token'
+};
+
+try
+{
+  var f = require('./config.json');
+  Object.keys(f).forEach(function(d)
+  {
+    config[d] = f[d];
+  });
+}
+catch (e)
+{
+}
+
+module.exports = config;
--- fbv/routes/index.js
+++ fbv/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var fitbit = require("fitbit-node");
+var config = require('../config');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/api/fitbit', function(req, res, next)
+{
+  res.json({});
 });
 
 module.exports = router;
