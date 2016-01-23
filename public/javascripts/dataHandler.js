46e5c6f5c39a49a7cc99a12fbafe347dcdaa04f3
osoken <osoken.devel@outlook.jp>
2016-01-23 20:07:21 +0900

    modified:   public/javascripts/dataHandler.js 	modified:   public/javascripts/lineChart.js 	modified:   views/member.jade

--- public/javascripts/dataHandler.js
+++ public/javascripts/dataHandler.js
@@ -1,7 +1,9 @@
 !(function(d3)
 {
   var dataHandler = {};
+  dataHandler.heartRateRaw = {};
   dataHandler.heartRate = {};
+  dataHandler.heartRateExtent = [0,1];
   var _dateFormat = d3.time.format('%Y-%m-%d');
   var _timeFormat = d3.time.format('%H:%M:%S');
   var _getHeartRate1d = function(dateStr, cb)
@@ -13,37 +15,38 @@
   {
     if ( from >= to )
     {
-      return cb(null, dataHandler.heartRate);
+      return cb(null, dataHandler.heartRateRaw);
     }
-    if ( dataHandler.heartRate[_dateFormat(from)] === void 0 )
+    if ( dataHandler.heartRateRaw[_dateFormat(from)] === void 0 )
     {
       _getHeartRate1d(_dateFormat(from), function(err, dat)
       {
         if (err != null)
         {
-          console.log(err);
-          return;
+          return cb(err,null);
         }
         if (dat['activities-heart-intraday'] === void 0 || dat['activities-heart-intraday']['dataset'] === void 0)
         {
-          console.log('invalid data');
-          return;
+          return cb(new Error('invalid data'),null);
         }
         var rec = d3.set();
-        dataHandler.heartRate[_dateFormat(from)] = [];
+        dataHandler.heartRateRaw[_dateFormat(from)] = [];
         dat['activities-heart-intraday']['dataset'].forEach(function(d)
         {
+          d.time = _timeFormat.parse(d.time);
           rec.add(d.time);
-          dataHandler.heartRate[_dateFormat(from)].push(d);
+          dataHandler.heartRateRaw[_dateFormat(from)].push(d);
         });
         var s = _timeFormat.parse('00:00:00').getTime();
         for ( var i = s; i < s+86400000; i+=60000 )
         {
           if (!rec.has(_timeFormat(new Date(i))))
           {
-            dataHandler.heartRate[_dateFormat(from)].push( {time: _timeFormat(new Date(i)), value: null} );
+            dataHandler.heartRateRaw[_dateFormat(from)].push( {time: new Date(i), value: null} );
           };
         }
+        dataHandler.heartRateRaw[_dateFormat(from)].sort(function(a,b){return (a.time < b.time)?-1:1;});
+        dataHandler.heartRateExtent[1] = Math.max( dataHandler.heartRateExtent[1], d3.max(dataHandler.heartRateRaw[_dateFormat(from)], function(d){return d.value;}) );
         dataHandler.getHeartRate( new Date(from.getTime() + 86400000), to, cb );
       });
     }
@@ -52,6 +55,5 @@
       dataHandler.getHeartRate( new Date(from.getTime() + 86400000), to, cb );
     }
   }
-  if (typeof define === "function" && define.amd) define(dataHandler); else if (typeof module === "object" && module.exports) module.exports = dataHandler;
-  this.dataHandler = dataHandler;
+  d3.dataHandler = dataHandler;
 }(d3));
